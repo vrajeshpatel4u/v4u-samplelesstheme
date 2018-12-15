16b088fdb29a3cc09e076c419dd2243bb5d1a7a1
vrajeshpatel4u <vrajeshpatel4u@gmail.com>
2018-12-15 10:55:45 +0530

    Magento 2 Sample Less Theme

---
+++ app/design/frontend/V4U/Samplelesstheme/registration.php
@@ -0,0 +1,8 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/V4U/Samplelesstheme',
+    __DIR__
+);
+?>
